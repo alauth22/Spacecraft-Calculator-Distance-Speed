4748704a8bda65f23b1790dfcbfdd237d0751764
25136574 <25136574@STUWIN10-17>
2023-09-06 12:32:49 -0500

    Add project files.

---
+++ Week 3_A/Week 3_A.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Week_3_A
+{
+    partial class frmExploreTheGalaxy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDays = new System.Windows.Forms.Label();
+            this.txtActualDays = new System.Windows.Forms.TextBox();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblActualSpeed = new System.Windows.Forms.Label();
+            this.lblDistance = new System.Windows.Forms.Label();
+            this.lblActualDistance = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDays
+            // 
+            this.lblDays.AutoSize = true;
+            this.lblDays.Location = new System.Drawing.Point(141, 45);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(114, 13);
+            this.lblDays.TabIndex = 0;
+            this.lblDays.Text = "Enter Number of Days:";
+            // 
+            // txtActualDays
+            // 
+            this.txtActualDays.Location = new System.Drawing.Point(267, 45);
+            this.txtActualDays.Name = "txtActualDays";
+            this.txtActualDays.Size = new System.Drawing.Size(100, 20);
+            this.txtActualDays.TabIndex = 1;
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(33, 81);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(222, 13);
+            this.lblSpeed.TabIndex = 2;
+            this.lblSpeed.Text = "Spacecraft Speed (MPD) by End of Last Day:";
+            // 
+            // lblActualSpeed
+            // 
+            this.lblActualSpeed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblActualSpeed.Location = new System.Drawing.Point(267, 80);
+            this.lblActualSpeed.Name = "lblActualSpeed";
+            this.lblActualSpeed.Size = new System.Drawing.Size(100, 23);
+            this.lblActualSpeed.TabIndex = 3;
+            // 
+            // lblDistance
+            // 
+            this.lblDistance.AutoSize = true;
+            this.lblDistance.Location = new System.Drawing.Point(38, 119);
+            this.lblDistance.Name = "lblDistance";
+            this.lblDistance.Size = new System.Drawing.Size(217, 13);
+            this.lblDistance.TabIndex = 4;
+            this.lblDistance.Text = "Total Distance Traveled by End of Last Day:";
+            // 
+            // lblActualDistance
+            // 
+            this.lblActualDistance.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblActualDistance.Location = new System.Drawing.Point(267, 119);
+            this.lblActualDistance.Name = "lblActualDistance";
+            this.lblActualDistance.Size = new System.Drawing.Size(100, 23);
+            this.lblActualDistance.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(52, 181);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 34);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(178, 181);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 34);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(306, 181);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 34);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmExploreTheGalaxy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 249);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblActualDistance);
+            this.Controls.Add(this.lblDistance);
+            this.Controls.Add(this.lblActualSpeed);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.txtActualDays);
+            this.Controls.Add(this.lblDays);
+            this.Name = "frmExploreTheGalaxy";
+            this.Text = "Explore The Galaxy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.TextBox txtActualDays;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblActualSpeed;
+        private System.Windows.Forms.Label lblDistance;
+        private System.Windows.Forms.Label lblActualDistance;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Week 3_A/Week 3_A.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_3_A
+{
+    public partial class frmExploreTheGalaxy : Form
+    {
+        public frmExploreTheGalaxy()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        { 
+            //Declare and Initialize a variable to hold the user input value for number of days. 
+            int days;  
+
+            //Obtain and validate if the variable days is of int type. 
+            if (int.TryParse(txtActualDays.Text, out days))
+            { 
+                //If days is of int type, need to validate if days is a non-zero, positive int value.
+                //Doing this to ensure user does not use "negative" days. 
+                if (days > 0)
+                {
+                    //Declare and Initialize variables for speed (MPD) and distance and our counter variable. 
+                    int DAY; //Counter variable to keep track of how many days through which we are looping. 
+                    int previous_speed = 1000; //Gave this value to help begin the for loop with correct value. 
+                    int current_speed;
+                    int current_distance;
+                    int total_distance;
+
+                    //Decided to hardcode just the first iteration before beginning the for loop. 
+                    //Decided this because you need specific values first before the iterations can begin. 
+                    current_speed = 1000; //Both values obtained from same formulae as in for loop. 
+                    total_distance = 500;
+                    
+                    //For loop now begins on day 2 since day 1 was done in lines 41 and 42.  
+                    for (DAY = 2; DAY <= days; DAY++)
+                    {
+                        //Calculate speed. 
+                        current_speed = previous_speed * 2; 
+
+                        //Calculate distance. 
+                        current_distance = (previous_speed + current_speed) / 2;
+
+                        //Calculate total distance. Need to continue to add to the total distance after each iteration.  
+                        total_distance += current_distance;
+
+                        //Update our previous speed to equal the current speed BEFORE next iteration.  
+                        previous_speed = current_speed;
+
+                    }
+
+                    //Display current_speed and total_distance values. 
+                    lblActualSpeed.Text = current_speed + " MPD";
+                    lblActualDistance.Text = total_distance + " Miles";
+                }
+
+                else
+                {
+                    //Error message to User if User types a zero or a negative integer. 
+                    MessageBox.Show("Invalid input. Please type only positive, non-zero integer values.");
+
+                    //I found this new method and decided to use it to clear the textbox!
+                    txtActualDays.Clear(); 
+
+                    //Puts Focus back on the textbox. 
+                    txtActualDays.Focus();
+                }
+
+            }
+
+            else
+            {
+                //Error Message to User if User types a non-integer value. 
+                MessageBox.Show("Invalid Input. Please type only an integer value.");
+                
+                //Clears the text from textbox. 
+                txtActualDays.Clear();
+
+                //Puts Focus back on the textbox. 
+                txtActualDays.Focus();
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //Clear all textboxes and puts Focus on the user input box. 
+            txtActualDays.Clear();
+            lblActualSpeed.Text = "";
+            lblActualDistance.Text = "";
+            txtActualDays.Focus();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Closes the form entirely. 
+            this.Close();
+        }
+    }
+}
